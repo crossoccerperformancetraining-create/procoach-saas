21012dad1c6b484b2c33dd57ff1705141844218f
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-07-22 23:48:03 -0300

    Add files via upload

---
+++ comissao-sw.js
@@ -0,0 +1,5 @@
+const CACHE='procoach-comissao-v1';
+const ASSETS=['/comissao.html','/comissao-manifest.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('procoach-comissao')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/comissao.html'))))});
