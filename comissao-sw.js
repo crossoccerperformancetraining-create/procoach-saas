daa25ac1b9892a4d5af3786839c9537c86095d42
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-07-23 13:00:08 -0300

    Delete comissao-sw.js

--- comissao-sw.js
+++
@@ -1,9 +0,0 @@
-const PREFIX='procoach-comissao';
-self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
-self.addEventListener('activate',event=>event.waitUntil((async()=>{
-  const keys=await caches.keys();
-  await Promise.all(keys.filter(k=>k.startsWith(PREFIX)).map(k=>caches.delete(k)));
-  await self.registration.unregister();
-  const clientsList=await self.clients.matchAll({type:'window',includeUncontrolled:true});
-  clientsList.forEach(client=>client.navigate('/index.html?v=113'));
-})()));
